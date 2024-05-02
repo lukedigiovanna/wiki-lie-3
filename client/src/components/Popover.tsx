880af9691f44e3f86b9e635ca5d913f0fa2a054b
lukedigiovanna <43895803+lukedigiovanna@users.noreply.github.com>
2024-05-02 00:05:38 -0400

    something

--- client/src/components/Popover.tsx
+++ client/src/components/Popover.tsx
@@ -10,14 +10,14 @@ const Popover: Component<PopoverProps> = (props: PopoverProps) => {
     const visible = () => props.visible;
 
     return (
-        <div class={`${!visible() && "hidden"} absolute left-0 top-0 w-[100vw] h-[100vh] flex flex-row justify-center`}>
+        <div class={`${!visible() && "hidden"} absolute left-0 top-0 w-full h-[100vh] flex flex-row justify-center`}>
             <div class="absolute bg-black opacity-50 w-full h-full left-0 top-0 z-40" onClick={props.close}>
             
             </div>
             <div class="relative rounded w-[75vw] mt-[20vh] max-w-96 h-fit z-50 p-4 bg-gray-50">
                 {props.children}
-                <button class="w-6 h-6 text-[0.7rem] text-center absolute right-[-12px] top-[-12px] rounded-full border border-gray-600 bg-gray-50 hover:bg-gray-300 active:bg-gray-400 transition align-middle" onClick={props.close}>
-                    ❌
+                <button class="w-6 h-6 text-[0.7rem] text-center absolute right-[-12px] top-[-12px] rounded-full border border-gray-600 bg-gray-50 hover:bg-gray-300 active:bg-gray-400 transition align-middle text-red-600" onClick={props.close}>
+                    ✘
                 </button>
             </div>
         </div>
